47a2a84bbf91c3f336f0ab03c88dca736b03bc82
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-25 15:23:04 -0500

    added views and routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,52 @@
+enable :sessions
+
+
 get '/' do
   @posts =Post.all
 
   erb :index
+end
+
+get '/user/:id' do
+  @user =User.find(params[:id])
+
+  erb :user
+end
+
+get '/post/:id/comments' do
+   @comments =Post.find(params[:id]).comments
+  erb :comments
+end  
+
+get '/user/:id/comments' do
+  @user =User.find(params[:id])
+  @comments= @user.comments
+  erb :user_comments
+end
+
+get '/submit' do
+  erb :submit
+end
+
+
+post '/login' do 
+
+    @user = User.find_by_email(params[:email])
+    if @user.password == params[:password]
+      session[:id] = @user.id
+      redirect "/"
+    else
+      redirect "/"
+    end
+
+end
+
+
+post '/submit' do
+  post = Post.create(params[:post])
+  post.user = User.find(session[:id])
+  post.save
+
+  redirect ("/post/#{post.id}/comments")
+
 end
