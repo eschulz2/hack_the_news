241c7ea52c1be0763ac92023d4b2e305e667a941
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-25 11:41:42 -0500

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,18 @@
 10.times do
+  new_user = User.new(name: Faker::Name.name, email: Faker::Internet.email)
+  new_user.password = "test"
+  new_user.password_confirmation = "test"
+  new_user.save
+
+end
+
+
+10.times do
+  Post.create(title: Faker::Lorem.words(5).join(" ").capitalize, url: Faker::Internet.url, user_id: 2)
+
+end
+
+100.times do
+  Comment.create(body: Faker::Company.bs + Faker::Company.bs + Faker::Company.bs + Faker::Company.bs,
+                user_id: rand(1..10), post_id: rand(1..10))
+end
